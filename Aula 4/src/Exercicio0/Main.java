c0a3c763ac502a4f51f08f595ea9ee2e12fb1cf2
OKawatani <eduardokawatani@gmail.com>
2026-02-28 08:43:14 -0300

    Mudanças de pasta

---
+++ Aula 4/src/Exercicio0/Main.java
@@ -0,0 +1,16 @@
+package Exercicio0;
+
+public class Main{
+
+    public static void main(String[] args)
+    {
+        Zumbi zumbi1 = new Zumbi();
+        Zumbi zumbi2 = new Zumbi();
+
+        zumbi1.nome = "Chris";
+        zumbi2.nome = "Lidia";
+
+        zumbi1.andar();
+        zumbi2.andar();
+    }
+}
---
+++ Aula 4/src/Exercicio0/Zumbi.java
@@ -0,0 +1,10 @@
+package Exercicio0;
+
+public class Zumbi{
+    String nome;
+
+    public void andar(){
+        System.out.println(nome + " está andando...");
+    }
+
+}
